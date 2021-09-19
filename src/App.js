c357f7a1fa28721f02ef546867a18d714062c927
Gabriel Espindola <gabrielbioinfo@gmail.com>
2021-09-19 10:47:39 -0300

    feat: adiciona as rotas no App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import NotFound from './pages/NotFound';
 import Wallet from './pages/Wallet';
 
 import './App.css';
@@ -14,6 +15,9 @@ function App() {
       </Route>
       <Route path="/carteira" exact>
         <Wallet />
+      </Route>
+      <Route path="*">
+        <NotFound />
       </Route>
     </Switch>);
 }
