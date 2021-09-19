bc660433318ec4a605f0b5bd01cbf6354a711b69
Gabriel Espindola <gabrielbioinfo@gmail.com>
2021-09-19 10:48:01 -0300

    fix: corrige chamada da store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
-import store from './reducers';
+import store from './store';
 
 import './index.css';
+
 ReactDOM.render(
   <BrowserRouter>
-    <Provider store={store}>
+    <Provider store={ store }>
       <App />
     </Provider>
   </BrowserRouter>,
